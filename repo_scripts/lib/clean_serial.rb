a94c7f16c763c9a2577e3a24a7d9647937b47678
Carl Mercier <foss@carlmercier.com>
2019-01-10 13:52:49 -0500

    Updated MY EFI

---
+++ repo_scripts/lib/clean_serial.rb
@@ -0,0 +1,26 @@
+require 'plist'
+require 'json'
+
+class CleanSerial
+  attr_reader :config_path
+
+  GENERATE_ME = "{{GENERATE ME}}"
+
+  def initialize(config_path)
+    @config_path = config_path
+  end
+
+  def clean!
+    result = Plist.parse_xml(config_path)
+
+    result['SMBIOS']['BoardSerialNumber'] = GENERATE_ME
+    result['SMBIOS']['SerialNumber'] = GENERATE_ME
+    result['SMBIOS']['SmUUID'] = GENERATE_ME
+    result['RtVariables']['MLB'] = "{{PASTE BOARD SERIAL NUMBER HERE}}"
+    result['Boot']['DefaultVolume'] = 'LastBootedVolume'
+    result['GUI']['Theme'] = 'embedded'
+
+    result.save_plist(config_path)
+    true
+  end
+end
