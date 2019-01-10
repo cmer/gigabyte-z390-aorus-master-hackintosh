bda974d094bf52b5e742afc30cbd7a8e25967fe1
Carl Mercier <foss@carlmercier.com>
2019-01-10 13:57:49 -0500

    Removed screen resolution

--- repo_scripts/lib/clean_config.rb
+++ repo_scripts/lib/clean_config.rb
@@ -1,7 +1,7 @@
 require 'plist'
 require 'json'
 
-class CleanSerial
+class CleanConfig
   attr_reader :config_path
 
   GENERATE_ME = "{{GENERATE ME}}"
@@ -19,6 +19,7 @@ class CleanSerial
     result['RtVariables']['MLB'] = "{{PASTE BOARD SERIAL NUMBER HERE}}"
     result['Boot']['DefaultVolume'] = 'LastBootedVolume'
     result['GUI']['Theme'] = 'embedded'
+    result['GUI']['ScreenResolution'] = ''
 
     result.save_plist(config_path)
     true
