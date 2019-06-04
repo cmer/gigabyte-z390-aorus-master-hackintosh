ab3a71323c4e92ef22c8b2e2cc90239ffbe30e78
Carl Mercier <foss@carlmercier.com>
2019-06-04 15:23:25 -0400

    Remove custom GUI entries on EFI dump

--- repo_scripts/lib/clean_config.rb
+++ repo_scripts/lib/clean_config.rb
@@ -5,6 +5,7 @@ class CleanConfig
   attr_reader :config_path
 
   GENERATE_ME = "{{GENERATE ME}}"
+  BOARD_SERIAL = "{{PASTE BOARD SERIAL NUMBER HERE}}"
 
   def initialize(config_path)
     @config_path = config_path
@@ -16,7 +17,7 @@ class CleanConfig
     result['SMBIOS']['BoardSerialNumber'] = GENERATE_ME
     result['SMBIOS']['SerialNumber'] = GENERATE_ME
     result['SMBIOS']['SmUUID'] = GENERATE_ME
-    result['RtVariables']['MLB'] = "{{PASTE BOARD SERIAL NUMBER HERE}}"
+    result['RtVariables']['MLB'] = BOARD_SERIAL
     result['Boot']['DefaultVolume'] = 'LastBootedVolume'
     result['GUI']['Theme'] = 'embedded'
     result['GUI']['ScreenResolution'] = ''
@@ -24,6 +25,7 @@ class CleanConfig
     result['GUI']['Scan']['Entries'] = true
     result['GUI']['Scan']['Legacy'] = true
     result['GUI']['Scan']['Linux'] = false
+    result['GUI'].delete('Custom')
 
     result.save_plist(config_path)
     true
